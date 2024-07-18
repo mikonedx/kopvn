aaa80c4966a421e98f020fc059f0bc199559dcc2
mikonedx <natsukidokii2002@gmail.com>
2024-07-19 03:48:32 +0700

    update

---
+++ app.js
@@ -0,0 +1,7 @@
+function handleClick(){
+    window.location.href = "index.html"
+}
+
+function register(){
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSdCXSJTCqI_P3n4Ut3NcNi8snPAxrh24yfITV0bpZR-G6leiw/viewform"
+}
