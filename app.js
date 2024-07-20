15d501e0aafb434d20a35067de03d5778cc045f7
S4ltl3ss <thaihoa040206@gmail.com>
2024-07-20 22:38:44 +0700

    update

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ function register(){
     window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSdCXSJTCqI_P3n4Ut3NcNi8snPAxrh24yfITV0bpZR-G6leiw/viewform"
 }
 
+function info(){
+    window.location.href = "info.html"
+}
+
 let sidebar = document.getElementById('bar');
 let resizeTimeout;
 let initialWidth = window.innerWidth;
