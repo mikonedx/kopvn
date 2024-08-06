47925c06e7a9e25d5edd063a9340add6df43b00c
S4ltl3ss <thaihoa040206@gmail.com>
2024-08-06 20:52:04 +0700

    fix

--- mappool.js
+++ mappool.js
@@ -5,7 +5,7 @@ const Qualifiers = [
         artist: "xi",
         diff: "12+",
         diffName: "Expert",
-        img: "https://cdn.discordapp.com/attachments/1180037189879472181/1266667012424466517/f6ac558d0e2ae1793829f8157fe0d415022118a861d629e7ac12d5734292681c.png?ex=66a5fb15&is=66a4a995&hm=5f48db9476f54a9e8c6158bdb0c9f5b3caa07f6b0a85b45e07d351bde3670fb0&",
+        img: "./Assets/image/Mjölnir.png",
     },
     {
         id: 2,
@@ -13,7 +13,7 @@ const Qualifiers = [
         artist: "s-don vs. 翡乃イスカ",
         diff: "12+",
         diffName: "Expert",
-        img: "https://cdn.discordapp.com/attachments/1180037189879472181/1266665122126888980/af5aa2985ef533cc430c6ed4ca9f0fd040dfb4f53650684b65b12c8f56e4fba1.png?ex=66a5f952&is=66a4a7d2&hm=50040052b126ed0d8e0b621c49945e33a33235fdd055acae7ddcf3fe96e4ca5d&",
+        img: "./Assets/image/Trrricksters.png",
     },
     {
         id: 3,
@@ -21,7 +21,7 @@ const Qualifiers = [
         artist: "削除",
         diff: "12+",
         diffName: "Expert",
-        img: "https://cdn.discordapp.com/attachments/1180037189879472181/1266665944256745616/e0ba440cc5576114c908ef379e21f9a43dc136cbafbaf91e998340d5d42eb2a7.png?ex=66a5fa16&is=66a4a896&hm=88c40ac9db04da5937686abb584cff8fd2a46d93293a0d8b08be04482892a303&",
+        img: "./Assets/image/Metamorphosism.png",
     },
 ];
 
@@ -32,7 +32,7 @@ const Round16 = [
         artist: "Camellia",
         diff: "13",
         diffName: "Expert",
-        img: "https://cdn.discordapp.com/attachments/1180037189879472181/1266674046293250078/3e7484824e1478afb450cd32b215334a8346da28fb8642abe588cc0ce4840dbc.png?ex=66a601a2&is=66a4b022&hm=5ad9bae54ef943e2e78c4471a69a5cfaf368916c39245d4079f48dfba2d6b32e&",
+        img: "./Assets/image/Azure.png",
     },
     {
         id: 2,
@@ -40,7 +40,7 @@ const Round16 = [
         artist: "rintaro soma",
         diff: "13+",
         diffName: "Expert",
-        img: "https://cdn.discordapp.com/attachments/1180037189879472181/1266674256515698739/3a914643f53b41ab5b3f0be0bb501b895a52660f8c78f8317a87a7669efc7930.png?ex=66a601d4&is=66a4b054&hm=6f1eb81cb419fbeb48e2d2e5b947d5d46879a8d7fef4be8378215b634454180f&",
+        img: "./Assets/image/Tsunagite.png",
     },
     {
         id: 3,
@@ -48,7 +48,7 @@ const Round16 = [
         artist: "EmoCosine",
         diff: "13+",
         diffName: "Master",
-        img: "https://cdn.discordapp.com/attachments/1180037189879472181/1266675121561538622/d04a44dcd14181db74d25e60effce0ee67d38a6647bdfbf2dee98d387be3eeeb.png?ex=66a602a2&is=66a4b122&hm=22026698a0036c3cd59fb586cae78bb43fadc42a89c71362041ce13d204b0ede&",
+        img: "./Assets/image/Magenta.png",
     },
 ];
 
@@ -59,7 +59,7 @@ const Round8 = [
         artist: "Endorfin.",
         diff: "13+",
         diffName: "Master",
-        img: "https://cdn.discordapp.com/attachments/1180037189879472181/1266675956488208384/116e4a0f6e4a3578cc0b794cd65c762c3f7c24e77a7b1fd160d8e610ddc06513.png?ex=66a60369&is=66a4b1e9&hm=20614401ef74eb7d1c070387aa869daf6117a36ecd3619f5c4472af5568bba2d&",
+        img: "./Assets/image/Alice.png",
     },
     {
         id: 2,
@@ -67,7 +67,7 @@ const Round8 = [
         artist: "ぬゆり",
         diff: "13+",
         diffName: "Master",
-        img: "https://cdn.discordapp.com/attachments/1180037189879472181/1266676304665641000/300e7986b3930da702a25f8267fbb0a14a5c843da109ab55749e7dd76d04d860.png?ex=66a603bc&is=66a4b23c&hm=31f1581eb5f73d3fefbf80fb7afb5f674378580d64c6591e7b3d4ecf0788db16&",
+        img: "./Assets/image/folern.png",
     },
     {
         id: 3,
@@ -75,7 +75,7 @@ const Round8 = [
         artist: "かいりきベア",
         diff: "13+",
         diffName: "ReMaster",
-        img: "https://cdn.discordapp.com/attachments/1180037189879472181/1266677121145769984/bc3de6138414088ef1c82f9c9d231cda5b2a9c42741569ab7454670e453cc1ca.png?ex=66a6047f&is=66a4b2ff&hm=231889563e0b4e3370f27ebdb0bb0ad0bfe4deb691e04ad9fee40f2d8e9f9632&",
+        img: "./Assets/image/Bug.png",
     },
 ];
 
